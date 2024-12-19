c2e5387a35fcbe2f65bb7f4d36f14e01d6c75705
StefanMogren <stefan_mogren@hotmail.com>
2024-12-19 11:01:37 +0100

    Made the function dynamic, added console.log explanations

--- float-converter.js
+++ float-converter.js
@@ -1,39 +1,38 @@
-let totalPrice = 8.075
-
-
-decimalRounding(8.075, 2);
+//decimalRounding(8.075, 2);
 decimalRounding(8.07458, 2);
+//decimalRounding(8.074586815, 8);
 
 
-// console.log(8.075 * 1000 === Math.round(8.075 * 1000))
+// The arguments are the number you want rounded and to how many decimals you want to round
+function decimalRounding(value, decimals) {
+    console.log("Initial value is " + value)
+    let decimalCounting = Math.pow(10, (decimals + 1))
+    let truncatedValue = Math.trunc(value)
+    let remainingValue = value - truncatedValue
+    console.log("Only the decimal value is " + remainingValue)
 
-function decimalRounding(value, numberOfDecimals) {
-    let remainingValue = Math.trunc(value)
-    let truncatedValue = value - remainingValue
-    console.log("Truncated value is " + truncatedValue)
-
-
-
-    let calculation = truncatedValue * Math.pow(10, (numberOfDecimals + 1))
-    console.log("Math power value is " +calculation)
+    let calculation = remainingValue * decimalCounting
+    console.log("Math power value is " + calculation)
 
    
     if (calculation.toString().indexOf('.99999') > -1 ) {
-        console.log("I have lots of nines!")
         calculation = Math.round(calculation)
-        console.log("I have rounded the value to " + calculation)
+        console.log("Since there are lots of nines, I have rounded the value to " + calculation)
     }
 
     if (calculation % 10 === 5) {
         calculation += 5;
-        calculation /= 1000
+        console.log("I manually added 5 to the value, resulting in " + calculation)
+        calculation /= decimalCounting
 
     } else {
         calculation /= 10;
         calculation = Math.round(calculation);
-        calculation /= 100;
+        console.log(calculation)
+        calculation /= decimalCounting / 10;
     }
-        calculation += remainingValue
+        console.log("The value before readding the truncated value is " + calculation)
+        calculation += truncatedValue
         console.log("The final value is " + calculation)
 
     
@@ -43,13 +42,14 @@ function decimalRounding(value, numberOfDecimals) {
     if (calculation % 10 === 5) {
         calculation += 5;
         calculation / Math.pow(10, (numberOfDecimals + 1))
-        console.log("End result is " + (calculation + truncatedValue))
+        console.log("End result is " + (calculation + remainingValue))
     } else {
         calculation = Math.round(calculation / 10)
         console.log("End result is " + calculation / Math.pow(10, (numberOfDecimals)))
     }
     */
 
-    //return calculation / e^(numberOfDecimals+1);
+    // console.log(8.075 * 1000 === Math.round(8.075 * 1000))
+    // return calculation / e^(numberOfDecimals+1);
 
 };
