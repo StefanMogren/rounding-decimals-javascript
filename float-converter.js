ee0042e017028671926428c4bba02bb7b2975e37
StefanMogren <stefan_mogren@hotmail.com>
2024-12-19 11:47:42 +0100

    Simplified the math in the else-statement

--- float-converter.js
+++ float-converter.js
@@ -1,7 +1,7 @@
 // The arguments are the number you want rounded and to how many decimals you want to round
 decimalRounding(8.075, 2);
 decimalRounding(8.07458, 2);
-//decimalRounding(8.074586815, 8);
+// decimalRounding(8.074586815, 8);
 
 
 function decimalRounding(value, decimals) {
@@ -10,25 +10,27 @@ function decimalRounding(value, decimals) {
     let truncatedValue = Math.trunc(value)
     let remainingValue = value - truncatedValue
     console.log("Only the decimal value is " + remainingValue)
-
+    
     let calculation = remainingValue * decimalCounting
     console.log("Math power value is " + calculation)
-
-   
+    
+    
     if (calculation.toString().indexOf('.99999') > -1 ) {
         calculation = Math.round(calculation)
         console.log("Since there are lots of nines, I have rounded the value to " + calculation)
     }
-
+    
     if (calculation % 10 === 5) {
         calculation += 5;
         console.log("I manually added 5 to the value, resulting in " + calculation)
         calculation /= decimalCounting
-                console.log("The value before adding back the truncated value is " + calculation)
+        console.log("The value before adding back the truncated value is " + calculation)
         calculation += truncatedValue
-
+        
     } else {
-        calculation = Math.round(value * (10 ** decimals)) / (10 ** decimals)
+        let test = Math.pow(10, decimals)
+        console.log("The edge case of decimals ending with 5 isn't an issue here, it's possible to use Math.round as normal.")
+        calculation = Math.round(value * test) / test
         /*
         calculation /= 10;
         calculation = Math.round(calculation);
@@ -36,24 +38,23 @@ function decimalRounding(value, decimals) {
         calculation /= decimalCounting / 10;
         */
     }
-
-        console.log("The final value is " + calculation)
-
     
-    
-    
-    /*
-    if (calculation % 10 === 5) {
-        calculation += 5;
-        calculation / Math.pow(10, (numberOfDecimals + 1))
-        console.log("End result is " + (calculation + remainingValue))
-    } else {
-        calculation = Math.round(calculation / 10)
-        console.log("End result is " + calculation / Math.pow(10, (numberOfDecimals)))
-    }
-    */
-
-    // console.log(8.075 * 1000 === Math.round(8.075 * 1000))
-    // return calculation / e^(numberOfDecimals+1);
-
+    console.log("The final value is " + calculation)
 };
+
+
+
+/*
+if (calculation % 10 === 5) {
+calculation += 5;
+calculation / Math.pow(10, (numberOfDecimals + 1))
+console.log("End result is " + (calculation + remainingValue))
+} else {
+calculation = Math.round(calculation / 10)
+console.log("End result is " + calculation / Math.pow(10, (numberOfDecimals)))
+}
+*/
+
+// console.log(8.075 * 1000 === Math.round(8.075 * 1000))
+// return calculation / e^(numberOfDecimals+1);
+
